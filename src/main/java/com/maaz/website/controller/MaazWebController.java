dc67e5c6e63efb57e3b93716d2327f1aba7f24f1
LAPTOP-E8SAGSSA\modani <modani130484@gmail.com>
2022-06-28 00:03:52 +0700

    -update code by modani

--- src/main/java/com/maaz/website/controller/MaazWebController.java
+++ src/main/java/com/maaz/website/controller/MaazWebController.java
@@ -1,6 +1,7 @@
 package com.maaz.website.controller;
 
 import com.maaz.website.consts.UrlPath;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpSession;
 
 @Controller
-public class MazzWebController {
+@Slf4j
+public class MaazWebController {
 
     @GetMapping(value = UrlPath.MAAZ_HOME)
     public String main(Model model, HttpSession session) {
