3283d2dbb17ffef85f850ceaab256e3a5d15e238
LAPTOP-E8SAGSSA\modani <modani130484@gmail.com>
2022-06-28 22:40:51 +0700

    -update code by modani

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,39 +1,26 @@
-$(() => {
+function configSwiper(params) {
     const thumbSwiper = new Swiper('.thumb-swiper', {
         slidesPerView: 5,        
         freeMode: false,
         watchSlidesProgress: true
     });
 
-    const mainswiper = new Swiper('.main-swiper', {
+    const _ = new Swiper('.main-swiper', {
         autoplay: {
             delay: 5000,
         },
+        slidesPerView: 3,
+        centeredSlides: true,
         loop: true,
         speed: 400,
-        spaceBetween: 100,
+        spaceBetween: 24,
         thumbs: {
             swiper: thumbSwiper
         },
         navigation: {
-            nextEl: ".main-swiper-next",
-            prevEl: ".main-swiper-prev",
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         }
-    });
-    updateSwiperStatus(mainswiper);
-    mainswiper.on('slideChange', function (e) {
-        updateSwiperStatus(e);
-    });
-
-    const serviceSwiper = new Swiper('.service-swiper', {
-        autoplay: { delay: 5000 },
-        loop: true, 
-        speed: 400,
-        spaceBetween: 100,
-        pagination: {
-            el: ".swiper-pagination",
-            dynamicBullets: true,
-        },
     });
 
     const recognitionSwiper = new Swiper('.recognitions-swiper', {
@@ -43,11 +30,20 @@ $(() => {
         slidesPerView: 4,
         spaceBetween: 16
     })
-});
-
-function updateSwiperStatus(swiper) {
-    var slidesCount = swiper.slides.length - 2;
-    var realIndex = swiper.activeIndex % slidesCount;
-    realIndex = realIndex || slidesCount;
-    $('.swiper-status').text(`${realIndex}/5`);
 }
+function configHover() {
+    $('[data-action="hover"]').hover(function () {
+        var target = $(this).data('target');
+        console.log(target);
+        $(target).addClass('active-hover');
+    }, function () {
+        var target = $(this).data('target');
+        console.log(target);
+        $(target).removeClass('active-hover');
+    });
+}
+
+$(() => {
+    configSwiper();
+    configHover();
+});
--- src/main/resources/static/js/stylesetup.js
+++ src/main/resources/static/js/stylesetup.js
@@ -3,15 +3,50 @@ $(() => {
 
     tailwind.config = {
         theme: {
-        extend: {
-            colors: {
-                color1: getComputedStyle(document.body ).getPropertyValue('--color1').trim(),
-                color2: getComputedStyle(document.body ).getPropertyValue('--color2').trim(),
-                color3: getComputedStyle(document.body ).getPropertyValue('--color3').trim(),
-                color4: getComputedStyle(document.body ).getPropertyValue('--color4').trim(),
-                color5: getComputedStyle(document.body ).getPropertyValue('--color5').trim(),
+            extend: {
+                colors: {
+                    color1: getComputedStyle(document.body ).getPropertyValue('--color1').trim(),
+                    color2: getComputedStyle(document.body ).getPropertyValue('--color2').trim(),
+                    color3: getComputedStyle(document.body ).getPropertyValue('--color3').trim(),
+                    color4: getComputedStyle(document.body ).getPropertyValue('--color4').trim(),
+                    color5: getComputedStyle(document.body ).getPropertyValue('--color5').trim(),
+                },
+                padding: {
+                    'section': 'var(--padding-section)',
+                    'col-gap': '1.5rem',
+                }
+            },
+            fontSize: {
+                'lg': ['44px', {
+                    lineHeight: '48px',
+                    fontWeight: '700'
+                }],
+                'hl': ['20px', {
+                    lineHeight: '24px',
+                    fontWeight: '400'
+                }],
+                'title': [
+                    '32px',
+                    {
+                        lineHeight: '40px',
+                    }
+                ],
+                'xl': ['60px', {
+                    lineHeight: '64px',
+                    fontWeight: '700'
+                }],
+                'item': ['20px', {
+                    lineHeight: '28px',
+                    fontWeight: '700'
+                }],
+                'cu': ['24px', {
+                    lineHeight: '36px'
+                }],
+            },
+            zIndex: {
+                '1': '1',
+                '2': '2',
             }
-        }
         }
     }
 });
